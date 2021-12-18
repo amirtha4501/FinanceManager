ff3030997147e9e15658d7246e96e89b72e7be0e
Amirtha <amirtha4501@gmail.com>
2021-12-18 11:23:52 +0530

    Added oauth to signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms'
 import { UserService } from '../services/user.service';
 import Swal from 'sweetalert2';
 import { ToastService } from '../services/toast.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
     selector: 'app-signup',
@@ -19,7 +20,8 @@ export class SignupComponent implements OnInit {
         private router: Router,
         private fb: FormBuilder,
         private toastService: ToastService,
-        private userService: UserService
+        private userService: UserService,
+        private authService: AuthService
     ) {
         this.signUp();
     }
@@ -71,5 +73,26 @@ export class SignupComponent implements OnInit {
                 }
             }
         )
+    }
+
+    oauthLogin(event: any): void {
+        console.log(event);
+        const oauthProvider: string = event.provider;
+        const authorizationCode: string = event.authorizationCode;
+        if (!authorizationCode) {
+            this.toastService.error('Login Failed');
+            this.toastService.error('Please try again!');
+            return;
+        }
+        this.authService.oAuthLogin(oauthProvider, { code: authorizationCode }).subscribe(
+            response => {
+                this.toastService.success(`Logged in using ${oauthProvider} successfully!`);
+                localStorage.setItem('token', response["accessToken"]);
+                this.router.navigate(['/desktop']);
+            },
+            errorResponse => {
+                this.toastService.error('Something went wrong');
+            }
+        );
     }
 }
